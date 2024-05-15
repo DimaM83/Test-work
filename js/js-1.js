4316feec0c08dd961ec6554d122200e46c362af8
DimaM83 <mdv16.01.83@gmail.com>
2024-05-15 02:28:10 +0200

    Update js-1.js

--- js/js-1.js
+++ js/js-1.js
@@ -1,40 +1,106 @@
-// const singleBtn = document.querySelector("#single");
+// console.log('Jacob Mercer');
+// console.log(26);
 
-// const handleClick = () => {
-//   console.log("click event listener callback");
-// };
+// let age = 25;
+// age = age + 1;
+// console.log(age);
 
-// singleBtn.addEventListener("click", handleClick);
+// const priceitem = 3500;
+// const orderedQuantity = 4;
 
-// // ===============================================
-// const multiBtn = document.querySelector("#multiple");
+// const totalprice = priceitem * orderedQuantity;
+// console.log(totalprice);
 
-// const firstCallback = () => {
-//   console.log("First callback!");
-// };
-// const secondCallback = () => {
-//   console.log("Second callback!");
-// };
-// const thirdCallback = () => {
-//   console.log("Third callback!");
-// };
-// const addCallback = () => {
-//     console.log("12345");
+
+// let a = 5;
+// let b = 10;
+// let c = 15;
+// let d = 20;
+
+// a = a + 2;
+// console.log(a);
+// b = b - 4;
+// console.log(b);
+// c = c * 3;
+// d = d / 10;
+
+
+// const username = 'Poly';
+// const message = "Welcome " + username + '!';
+// console.log(message);
+
+
+// let username = "Poly";
+// let message = `Username ${username} is ${username.length} characters long`;
+// console.log(message);
+// username = "Harambe";
+// message = `Username ${username} is ${username.length} characters long`;
+// console.log(message);
+
+
+
+// const courseTopic = "JavaScript essentials";
+
+// const courseTopicLength = courseTopic;
+// const firstElement = courseTopic;
+// const lastElement = courseTopic;
+
+
+
+// const age = 16;
+// let isAdult = age <= 18;
+// console.log(isAdult);
+
+
+// const correctPassword = "jqueryismyjam";
+// const userPassword = "mangodab3st";
+// const isValid = correctPassword === userPassword;
+// console.log(isValid);
+
+
+// const value = '24.5px';
+// const numerical = Number.parseFloat(value);
+// console.log(numerical);
+
+
+// console.log(Math.ceil(1.1));
+
+
+// function sayHi() {
+//     console.log('Hello, this is my first function!');
 // }
-
-// multiBtn.addEventListener("click", firstCallback);
-// multiBtn.addEventListener("click", secondCallback);
-// multiBtn.addEventListener("click", thirdCallback);
-// multiBtn.addEventListener("click", addCallback);
+// sayHi();
 
 
-// const dog = {
-//   name: "Mango",
-//   age: 3,
-//   ostGoodBoy: true,
-// };
-
-// const json = JSON.stringify(dog);
-// console.log(json);
+// function add(a, b, c) {
+// console.log(`Addition result equals ${a + b + c}`);
+// }
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
 
 
+// function add(a, b, c) {
+//     return a + b + c;
+// }
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
+
+
+// function makeMessage(name, price) {
+//     return `You picked ${name}, price per item is ${price} credits`
+// }
+// makeMessage('Radar', 6150);
+// makeMessage('Scanner', 3500);
+// makeMessage('Reactor', 8000)
+// makeMessage('Engine', 4070);
+
+function calculateTotalPrice(orderedQuantity, pricePerItem) {
+    return orderedQuantity * pricePerItem;
+}
+calculateTotalPrice(5, 100);
+calculateTotalPrice(8, 60);
+calculateTotalPrice(3, 400);
+calculateTotalPrice(1, 3500);
+calculateTotalPrice(12, 70);
