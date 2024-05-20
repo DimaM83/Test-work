05c46b777b6c0670077ae59e4f984b2a1f66e8b4
DimaM83 <mdv16.01.83@gmail.com>
2024-05-20 21:43:43 +0200

    Update js-1.js

--- js/js-1.js
+++ js/js-1.js
@@ -129,3 +129,40 @@
 // console.log(totalPrice);
 
 
+
+
+                         
+                         // HW-1
+                            
+// function makeTransaction(quantity, pricePerDroid) {
+//     const totalPrice = quantity * pricePerDroid;
+//     return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+// }
+// console.log(makeTransaction(5, 3000));
+// console.log(makeTransaction(3, 1000));
+// console.log(makeTransaction(10, 500));
+
+
+
+// function getShippingMessage(country, price, deliveryFee) {
+//     const totalPrice = price + deliveryFee;
+//     return `Shipping to ${country} will cost ${totalPrice} credits`;
+// }
+// console.log(getShippingMessage('Australia', 120, 50));
+// console.log(getShippingMessage('Germany', 80, 20));
+// console.log(getShippingMessage('Sweden', 100, 20));
+
+
+
+function getElementWidth(content, padding, border) {
+   const contentWidth = parseFloat(content);
+   const paddingWidth = parseFloat(padding);
+   const borderWidth = parseFloat(border);
+
+    const allWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
+
+    return allWidth;
+}
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px")); 
+console.log(getElementWidth("200px", "0px", "0px"));
